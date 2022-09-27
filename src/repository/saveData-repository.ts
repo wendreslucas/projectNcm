f28ee4eadd34d1ee9826adc7d8b0be51e459cbe9
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2022-09-27 14:20:40 -0300

    implementação saveData-repository

---
+++ src/repository/saveData-repository.ts
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+import Data from '../models';
+import { Request } from 'express';
+
+const saveDataNcm = (req: Request) => {
+  const {
+    codigo,
+    descricao,
+    data_inicio,
+    data_fim,
+    tip_ato,
+    numero_ato,
+    ano_ato,
+  } = req.body;
+  const newData = new Data.ncm({
+    _id: new mongoose.Types.ObjectId(),
+    codigo,
+    descricao,
+    data_inicio,
+    data_fim,
+    tip_ato,
+    numero_ato,
+    ano_ato,
+  });
+  return newData.save();
+};
+
+export default {
+  saveDataNcm,
+};
