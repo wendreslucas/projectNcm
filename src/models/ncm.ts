14502ec063b245c6579956cf4a2423a9d7753f24
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2022-09-27 11:09:39 -0300

    implementação model ncm

---
+++ src/models/ncm.ts
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from 'mongoose';
+import INcm from '../types/INcm';
+
+const ncmSchema: Schema = new mongoose.Schema(
+  {
+    codigo: {
+      type: String,
+      required: 'Insira um código válido',
+    },
+    descricao: {
+      type: String,
+      required: 'Insira uma descricao',
+    },
+    data_inicio: {
+      type: String,
+      required: 'Insira uma data inicio',
+    },
+    data_fim: {
+      type: String,
+      required: 'Insira uma data fim',
+    },
+    tipo_ato: {
+      type: String,
+      required: 'Insira o tipo do ato',
+    },
+    numero_ato: {
+      type: String,
+      required: 'Insira o numero do ato',
+    },
+    ano_ato: {
+      type: String,
+      required: 'Insira o ano do ato',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.model<INcm>('INcm', ncmSchema);
