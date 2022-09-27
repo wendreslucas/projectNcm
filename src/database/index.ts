4dba1d2bcce38cfd8d8837debf84b6d4f241ff6d
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2022-09-27 10:01:09 -0300

    implementação conexão db

---
+++ src/database/index.ts
@@ -0,0 +1,18 @@
+require('dotenv').config();
+import mongoose from 'mongoose';
+
+function connectToDatabase() {
+  mongoose
+    .connect(process.env.MONGO_URL)
+    .then((data) => {
+      console.log('Mongoose Connected');
+    })
+    .catch((err) => {
+      console.log('Error connecting to Mongoose', err.message);
+    });
+  const database = mongoose.connection;
+  database.on('error', (err) => console.error(err.message));
+  // database.once('open', () => console.log('Connected to database))
+}
+
+export default connectToDatabase;
