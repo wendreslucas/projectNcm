c8707705efcbe823a4d727a8be60305274adcfbc
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2022-09-27 10:52:31 -0300

    implementação server, router

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import connectToDatabase from './database';
+import router from './routes/router';
+
 const app = express();
 const PORT = process.env.PORT || 8080;
 const cors = require('cors');
@@ -7,6 +9,7 @@ const cors = require('cors');
 connectToDatabase();
 app.use(cors());
 app.use(express.json());
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
