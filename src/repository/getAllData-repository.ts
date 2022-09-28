ea1fa9f9fb6b54e77e3124b392808874284101d0
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2022-09-28 11:06:16 -0300

    alteração do limite de visualização para 20

--- src/repository/getAllData-repository.ts
+++ src/repository/getAllData-repository.ts
@@ -2,7 +2,7 @@ import Data from '../models';
 import { Request } from 'express';
 
 const getAllDataNcm = (req: Request) => {
-  return Data.ncm.find({}).limit(50);
+  return Data.ncm.find({}).limit(20);
 };
 
 export default { getAllDataNcm };
